508b1de333541e8a60acdef58c5a52727ed4cca2
Leonardo Nonino <lnonino92@gmail.com>
2025-07-14 18:35:46 -0300

    empezando entrega final

--- js/inicio.js
+++ js/inicio.js
@@ -1,94 +1,113 @@
-let usuariosRecuperados = localStorage.getItem("arrayDeUsuarios")
-let Usuarios = JSON.parse(usuariosRecuperados)
 let totalinversion
+const URLUsuario = "./db//usuario.json"
+let usuarioLoggeado = ""
 
-const usuarioOn = localStorage.getItem("usuarioOn");
-const usuarioLoggeado = JSON.parse(usuarioOn);
-
-const carterasJSON = localStorage.getItem("arrayDeCarteras");
-const Carteras = JSON.parse(carterasJSON);
-
-let OrdenesJSON = localStorage.getItem("arrayDeOrdenes");
-let Ordenes = JSON.parse(OrdenesJSON);
-
-const indexUsuario = Usuarios.findIndex(usuario => usuario.nombreUsuario === usuarioLoggeado.nombreUsuario);
-
-const OrdenesOn = Ordenes[indexUsuario]
-
-function mostrarOrdenes(OrdenesOn){
+function obtenerUsuario(){
+    fetch(URLUsuario)
+        .then(response => response.json())
+        .then(data => {usuarioLoggeado = data;
+        })
+    }
     
-    const contenedorOrdenes = document.getElementById("divMisOrdenes")
+    usuarioLoggeado = obtenerUsuario()
+    
+    const spane = document.getElementById("spane")
+    
+    
 
-    OrdenesOn.forEach(e =>{
+const divMisOrdenes = document.getElementById("divMisOrdenes")
+const colorOrdenes = document.getElementsByClassName("ordenesUsuario")
+
+const URLOrdenes = "./db/dataOrdenes.json"
+
+function obtenerOrdenes(){
+    fetch(URLOrdenes)
+    .then(response => response.json())
+    .then(data => renderOrdenes(data))
+
+}
+
+obtenerOrdenes()
+
+function renderOrdenes(ListaOrdenes) {
+    ListaOrdenes.forEach(orden =>{
         const ordenes = document.createElement("section")
         ordenes.classList.add("sectionMisOrdenes")
         ordenes.style.borderBottom = "1px solid grey";
 
         ordenes.innerHTML = `
-            <section>${e.Nombre}</section>
-            <section>${e.ticker}</section>
-            <section>$${e.precio}</section>
-            <section>${e.cantidad}</section>
-            <section class="ordenesUsuario">${e.orden}</section>
-           
-        `
-    
-        contenedorOrdenes.appendChild(ordenes);
-
+            <section>${orden.Nombre}</section>
+            <section>${orden.ticker}</section>
+            <section>$${orden.precio}</section>
+            <section>${orden.cantidad}</section>
+            <section class="ordenesUsuario">${orden.orden}</section>
+            
+            `
+            
+            divMisOrdenes.appendChild(ordenes);
+            
         const tipoOrden = ordenes.querySelector(".ordenesUsuario");
-        if (e.orden === "compra") {
+        if (orden.orden === "compra") {
             tipoOrden.style.color = "green";
-        } else if (e.orden === "venta") {
+        } else if (orden.orden === "venta") {
             tipoOrden.style.color = "red";
-        }
-    });
-}
+        } 
+    })
 
-
-
-const colorOrdenes = document.getElementsByClassName("ordenesUsuario")
-
-
-if (OrdenesOn && OrdenesOn.length > 0) {
-    mostrarOrdenes(OrdenesOn);
+};
+if (divMisOrdenes.length > 0) {
+    renderOrdenes(ListaOrdenes);
 } else {
     const ordenesVacia = document.createElement("section");
     ordenesVacia.classList.add("noHayOrdenes");
     ordenesVacia.innerHTML = `<span>No existen órdenes que mostrar</span>`;
-    contenedorOrdenes.appendChild(ordenesVacia);
+    divMisOrdenes.appendChild(ordenesVacia);
+}  
+
+
+const listaCedears = document.getElementById("divInversiones")
+
+const URLCatera = "./db/data.json"
+
+function ObtenerCartera() {
+    fetch(URLCatera)
+    .then(response => response.json())
+    .then(data => {
+            renderCartera(data);
+  
+            const totalInversion = data.reduce((acum, cedear) => {
+                return acum + (cedear.cantidad * cedear.precio);
+            }, 0);
+
+            const totalInversiones = document.getElementById("spanTotalInvertido")
+            totalInversiones.innerText = "$" + totalInversion.toFixed(2);
+            totalInversiones.innerText = "$" + totalInversion 
+        })
+        
 }
 
 
+ObtenerCartera()
 
-const CarteraOn = Carteras[indexUsuario]
 
-function mostrarInversiones(CarteraOn){
+function renderCartera(ListaCedears) {
 
-    const contenedorInversiones = document.getElementById("divInversiones");
-
-    CarteraOn.forEach((e) => {
+    ListaCedears.forEach(cedear => {
         const lineas = document.createElement("section");
         lineas.classList.add("sectionInversion")
         lineas.style.borderBottom = "1px solid grey";
 
         lineas.innerHTML = `
-            <section>${e.Nombre}</section>
-            <section>${e.ticker}</section>
-            <section>$${e.precio}</section>
-            <section>${e.cantidad}</section>
-            <section>$${e.cantidad * e.precio}</section>
+            <section>${cedear.Nombre}</section>
+            <section>${cedear.ticker}</section>
+            <section>$${cedear.precio}</section>
+            <section>${cedear.cantidad}</section>
+            <section>$${cedear.cantidad * cedear.precio}</section>
             <section class="inversionesVenderComprar"><a href="./ordenes.html" class="inversionesComprar">Comprar</a><a href="./ordenes.html" class="inversionesVender">Vender</a></section>
         `;
-
-        contenedorInversiones.appendChild(lineas);
+        listaCedears.appendChild(lineas);
     });
 }
 
-const MiCartera = mostrarInversiones(CarteraOn);
-const totalInversion = CarteraOn.reduce((acumulador, e) => {
-    return acumulador + (e.cantidad * e.precio);
-}, 0);
 
-const totalInversiones = document.getElementById("spanTotalInvertido")
- totalInversiones.innerText = "$" + totalInversion
- 
+
